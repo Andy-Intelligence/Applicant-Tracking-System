054e902b010532411d38aee783fd99e95d0a5f12
Taiwo AKerele <davcodes24@gmail.com>
2023-10-24 13:39:53 +0100

    working on the login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import Image from "next/image";
+import illustration from "./images/Rectangle 7.svg";
 export default function Home() {
   return (
-    
-      <h1 className="text-center pt-8 text-4xl font-bold">ATS project</h1>
-  )
+    <div>
+      <Image src={illustration} alt="logo" quality={100} className="img" />
+      <div className="absolute top-6 left-0  floater ">
+        <p className="text-white  font-semibold text-2xl text-center">
+          Streamline Your Recruitment Effort with{" "}
+          <span className="block">our Advanced Application Tracking System </span>
+        </p>
+      </div>
+    </div>
+  );
 }
