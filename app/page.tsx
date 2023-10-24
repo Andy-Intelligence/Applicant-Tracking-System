130727905ee74f66c0c39d81de7ff2c36c980d7f
Taiwo AKerele <davcodes24@gmail.com>
2023-10-24 13:48:33 +0100

    Added the bottom

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import illustration from "./images/Rectangle 7.svg";
 export default function Home() {
   return (
     <div>
+      <div>
       <Image src={illustration} alt="logo" quality={100} className="img" />
-      <div className="absolute top-6 left-0  floater ">
+      <div className="absolute bottom-6 left-0  floater ">
         <p className="text-white  font-semibold text-2xl text-center">
           Streamline Your Recruitment Effort with{" "}
           <span className="block">our Advanced Application Tracking System </span>
         </p>
+      </div>
       </div>
     </div>
   );
