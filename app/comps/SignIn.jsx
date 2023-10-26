2ee6831feec89f9bb85e9647cc8fa28dc2eb080b
Taiwo AKerele <davcodes24@gmail.com>
2023-10-26 15:08:47 +0100

    Added the sigin method

--- app/comps/SignIn.jsx
+++ app/comps/SignIn.jsx
@@ -1,35 +1,41 @@
-'use client';
+"use client";
 import Image from "next/image";
-import Google from '../images/continue-with-google-centre-google-logo.svg'
-import Facebook from '../images/continue-with-facebook-centre-facebook-logo.svg';
-
+import Google from "../images/continue-with-google-centre-google-logo.svg";
+import Facebook from "../images/continue-with-facebook-centre-facebook-logo.svg";
+import { UserAuth, } from "../context/AuthContext";
 const SignIn = () => {
-    
-    const handleClick = () => {
-        alert('Taiwo')
+    const { user, googleSignIn, logOut } = UserAuth();
+
+  const handleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <>
-            <button onClick={handleClick} className="google">
-                <Image 
-                    src={Google}
-                    alt="Sign in with Google"
-                    quality={100}
-                    width={20}
-                />
-                <p>Sign Up with Google</p>
-            </button>
-            <button className="facebook">
-                <Image 
-                    src={Facebook}
-                    alt="Sign in with Google"
-                    quality={100}
-                    width={20}
-                />
-                <p>Sign Up with Google</p>
-            </button>
-        </>
-    );
-}
+  
+  }
+  return (
+    <>
+      <button onClick={handleSignIn} className="google">
+        <Image
+          src={Google}
+          alt="Sign in with Google"
+          quality={100}
+          width={20}
+        />
+        <p>Sign Up with Google</p>
+      </button>
+      <button className="facebook">
+        <Image
+          src={Facebook}
+          alt="Sign in with Google"
+          quality={100}
+          width={20}
+        />
+        <p>Sign Up with Google</p>
+      </button>
+    </>
+  );
+};
 
 export default SignIn;
