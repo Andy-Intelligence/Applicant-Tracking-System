c7eb1c1b7dda3e477dfc77a8742f4779589bec2b
Taiwo AKerele <davcodes24@gmail.com>
2023-11-03 15:49:23 +0100

    done with sign-up

--- app/Login/Company.jsx
+++ app/Login/Company.jsx
@@ -1,38 +1,87 @@
-'use client'
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-const Company = () => {
-    const router = useRouter();
-    const [fNameValue, setFNameValue] = useState('');
-    const [cNameValue, setCNameValue] = useState('');
-    const [cMailValue, setCMailValue] = useState('');
-    const [passwordValue, setPasswordValue] = useState('');
-    const [routing, setRouting] = useState('/Login');
-    const userData = () => {
-        if(!fNameValue === '' && !cNameValue === '' && !cMailValue === '' && !passwordValue === '') {
-            localStorage.setItem('name', setFNameValue)
-            localStorage.setItem('CompanyName', setCNameValue)
-            localStorage.setItem('CompanyMail', setCMailValue)
-            localStorage.setItem('password', setPasswordValue)
-            setRouting('/')
-        }
-        else {
-            alert('input the necessary details');
-            router.push('/Login')
-            
-        }
-    }
+"use client";
+import React, { useState } from "react";
 
-    return (
-        <form className="width-1/2" id="contact-form">
-            <input  type="text" placeholder="Full Name" required id="fName"/>
-            <input type="text" placeholder="Company Name" required id="CName"/>
-            <input type="email" id="Cmail" required placeholder="Company Email" />
-            <input type="password" id="password" required placeholder="Password" />
-            <Link href={routing}><input type="submit" onClick={userData} value="Continue" id="submit" className="text-white bold" /></Link>
-        </form>
-    );
-}
+import Link from "next/link";
+/**
+ * Renders a form for the user to enter their information.
+ * @returns {JSX.Element} The rendered form component.
+ */
+const Company = () => {
+  const [inputs, setInputs] = useState({
+    fName: "",
+    CName: "",
+    CMail: "",
+    password: "",
+  });
+
+  const [routing, setRouting] = useState("/Login");
+
+  /**
+   * Handles form submission.
+   */
+  const handleSubmit = () => {
+    if (
+      inputs.fName === "" ||
+      inputs.CName === "" ||
+      inputs.CMail === "" ||
+      inputs.password === ""
+    ) {
+      alert("Please fill in all fields");
+    } else {
+      setRouting("/");
+    }
+  };
+
+  /**
+   * Handles input changes.
+   * @param {React.ChangeEvent<HTMLInputElement>} event - The input change event.
+   */
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setInputs((prevInputs) => ({ ...prevInputs, [id]: value }));
+    localStorage.setItem("formInputs", JSON.stringify(inputs));
+  };
+
+  return (
+    <form className="width-1/2" id="contact-form">
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Full Name"
+        required
+        id="fName"
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Company Name"
+        required
+        id="CName"
+      />
+      <input
+        type="email"
+        onChange={handleChange}
+        id="CMail"
+        required
+        placeholder="Company Email"
+      />
+      <input
+        type="password"
+        onChange={handleChange}
+        id="password"
+        required
+        placeholder="Password"
+      />
+      <Link href={routing} onClick={handleSubmit}>
+        <input
+          type="submit"
+          value="Continue"
+          id="submit"
+          className="text-white bold"
+        />
+      </Link>
+    </form>
+  );
+};
 
 export default Company;
